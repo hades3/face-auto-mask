2603ac2ef891dc6bd328c3cb77cb62c1b29ecb22
Yeongchan Kim <hades1@kakao.com>
2024-12-31 00:13:46 +0900

    feat: 얼굴 인식 구현

--- src/main.py
+++ src/main.py
@@ -1,4 +1,31 @@
 import numpy as np
 import mediapipe as mp
 import cv2
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
 
+cap = cv2.VideoCapture('../resources/talking.mp4')
+with mp_face_detection.FaceDetection(
+        model_selection=0, min_detection_confidence=0.5) as face_detection:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            break
+
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = face_detection.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.detections:
+            for detection in results.detections:
+                mp_drawing.draw_detection(image, detection)
+
+        cv2.imshow('Face Detection', cv2.resize(image, None, fx=0.5, fy=0.5))
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
